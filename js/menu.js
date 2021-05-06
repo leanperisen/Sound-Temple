2616364de0aa92a697dc740f48e6542608ab6e45
Leandro Mariperisena <leandro.mariperisena@gmail.com>
2021-05-05 23:07:37 -0300

    Se estuvieron realizando modificaciones en el nav, para poder adaptarlo a la estructura necesaria para implementar el menú responsive. También, se implementó el menú responsive para pantallas inferiores a 720px

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".nav__toggle");
+const menu = document.querySelector(".menu");
+
+navToggle.addEventListener("click", () => {
+    menu.classList.toggle("menu--visible");
+})
