99ea2660a0d2d2c5de2003e1f163b4c65b374294
Leandro Mariperisena <leandro.mariperisena@gmail.com>
2021-05-09 07:49:42 -0300

    Se agregó SEO a la página (keywords, decription, cambios en titles). También se estuvieron haciendo algunos ajustes a clases que BS pisaba y se agregó el Favicon del sitio

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelector(".nav__toggle");
 const menu = document.querySelector(".menu");
 
+
 navToggle.addEventListener("click", () => {
     menu.classList.toggle("menu--visible");
 })
